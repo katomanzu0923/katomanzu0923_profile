0502087fce459c0884cc4b48e5ad089cb682d690
katomanzu0923 <katomanzu86@gmail.com>
2020-10-31 17:15:42 +0900

    have built a layout in programing component.

--- src/components/Programing.vue
+++ src/components/Programing.vue
@@ -3,14 +3,13 @@
 		<h1>フロントエンド</h1>
 		<div class="front">
 			<div class="html">
-				<div class="html_top">
+				<div class="html_main">
 					HTML
 				</div>
 				<div class="html_content">
-					<h3>学んだ内容</h3>
-					<p>私がHTMLで学んだことは以下のことです<br>
+					私がHTMLで学んだことは以下のことです.<br>
 					学んだ内容としては以下のようなことを学びました<br>
-					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします</p>
+					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします
 				</div>
 			</div>
 			<div class="css">
@@ -18,19 +17,17 @@
 					CSS
 				</div>
 				<div class="css_content">
-					<h3>学んだ内容</h3>
-					<p>私がHTMLで学んだことは以下のことです<br>
+					私がHTMLで学んだことは以下のことです<br>
 					学んだ内容としては以下のようなことを学びました<br>
-					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします</p>
+					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします
 				</div>
 			</div>
 			<div class="scss">
 				<div class="scss_top">SCSS</div>
 				<div class="scss_content">
-					<h3>学んだ内容</h3>
-					<p>私がscssで学んだことは以下のことです<br>
+					私がscssで学んだことは以下のことです<br>
 					学んだ内容としては以下のようなことを学びました<br>
-					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします</p>
+					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします
 				</div>
 			</div>
 			<div class="javascript">
@@ -38,21 +35,18 @@
 					Javascript
 				</div>
 				<div class="javascript_content">
-					<h3>学んだ内容</h3>
-					<p>私がjavascriptで学んだことは以下のことです<br>
+					私がjavascriptで学んだことは以下のことです<br>
 					学んだ内容としては以下のようなことを学びました<br>
-					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします</p>
-				</div>
+					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします				</div>
 			</div>
 			<div class="jquery">
 				<div class="jquery_top">
 					jQuery
 				</div>
 				<div class="jquery_content">
-					<h3>学んだ内容</h3>
-					<p>私がjqueryで学んだことは以下のことです<br>
+					私がjqueryで学んだことは以下のことです<br>
 					学んだ内容としては以下のようなことを学びました<br>
-					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします</p>
+					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします
 				</div>
 			</div>
 			<div class="vue">
@@ -60,25 +54,23 @@
 					Vue.js
 				</div>
 				<div class="vue_content">
-					<h3>学んだ内容</h3>
-					<p>私がvueで学んだことは以下のことです<br>
+					私がvueで学んだことは以下のことです<br>
 					学んだ内容としては以下のようなことを学びました<br>
-					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします</p>
+					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします
 				</div>
 			</div>
 
 		</div>
-		<h1>サーバーサイド</h1>
+		<h2>サーバーサイド</h2>
 		<div class="server"> 
 			<div class="ruby">
 				<div class="ruby_top">
 					Ruby
 				</div>
 				<div class="ruby_content">
-					<h3>学んだ内容</h3>
-					<p>私がRubyで学んだことは以下のことです<br>
+					私がRubyで学んだことは以下のことです<br>
 					学んだ内容としては以下のようなことを学びました<br>
-					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします</p>
+					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします
 				</div>
 			</div>
 			<div class="mysql">
@@ -86,10 +78,9 @@
 					mysql
 				</div>
 				<div class="mysql_content">
-					<h3>学んだ内容</h3>
-					<p>私がmysqlで学んだことは以下のことです<br>
+					私がmysqlで学んだことは以下のことです<br>
 					学んだ内容としては以下のようなことを学びました<br>
-					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします</p>
+					詳しkは以下のgithubにて多くのhtmlコードがありますのでご確認お願いします
 				</div>
 			</div>
 		</div>
@@ -109,120 +100,241 @@
 		justify-content: space-between;
 		/*padding: 5%;*/
 	}
+	h1 {
+		text-align: center;
+    width: 60%;
+		position: relative;
+    display: inline-block;
+    padding: 0 5px;
+		margin-bottom: 5%;
+		color: #ff4618;
+	}
+	h1::before {
+		position: absolute;
+    content: '';
+    top: 0;
+    right: 5px;
+    width: 5px;
+    height: 100%;
+    border: 5px solid #ff7c5c;
+    border-left: none;
+	}
+ h1::after {
+		position: absolute;
+    content: '';
+    top: 0;
+    left: 5px;
+    width: 5px;
+    height: 100%;
+    border: 5px solid #ff7c5c;
+    border-right: none;
+		margin-bottom: 5%;
+	}
 	.html {
 		flex-basis: 40%;
 		position: relative;
+    display: block;
+    padding: 0.5em 1em;
+    text-decoration: none;
+    background: #f7f7f7;
+    border-top: solid 6px #ff7c5c;/*左線*/
+    color: #ff7c5c;/*文字色*/
+    font-weight: bold;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
 	}
-	.html_top {
-		position: absolute;
-		top: -4%;
-		width: 30%;
-		left: 35%;
+	.html_main {
+		background: transparent;
 	}
 	.html_content {
-		border:solid red;
+		background: transparent;
+		padding: 10%;
+		text-align: start;
 	}
 	.css {
 		flex-basis: 40%;
 		position: relative;
+		display: block;
+    padding: 0.5em 1em;
+    text-decoration: none;
+    background: #f7f7f7;
+    border-top: solid 6px #ff7c5c;/*左線*/
+    color: #ff7c5c;/*文字色*/
+    font-weight: bold;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
 	}
 	.css_top {
-		position: absolute;
-		top: -4%;
-		width: 25%;
-		left: 37.5%;
+		background: transparent;
 	}
 	.css_content {
-		border:solid rgb(0, 247, 255);
+		background: transparent;
+		padding: 10%;
+		text-align: start;
 	}
 	.scss {
 		flex-basis: 40%;
 		position: relative;
+		display: block;
+    padding: 0.5em 1em;
+    text-decoration: none;
+    background: #f7f7f7;
+    border-top: solid 6px #ff7c5c;/*左線*/
+    color: #ff7c5c;/*文字色*/
+    font-weight: bold;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
 		margin-top: 5%;
 	}
+
 	.scss_top {
-		position: absolute;
-		top: -4%;
-		width: 30%;
-		left: 35%;
+		background: transparent;
 	}
 	.scss_content {
-		border:solid rgb(255, 80, 226);
+		background: transparent;
+		padding: 10%;
+		text-align: start;
 	}
 	.javascript {
 		flex-basis: 40%;
 		position: relative;
+		display: block;
+    padding: 0.5em 1em;
+    text-decoration: none;
+    background: #f7f7f7;
+    border-top: solid 6px #ff7c5c;/*左線*/
+    color: #ff7c5c;/*文字色*/
+    font-weight: bold;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
 		margin-top: 5%;
 	}
 	.javascript_top {
-		position: absolute;
-		top: -4%;
-		width: 50%;
-		left: 25%;
+		background: transparent;
 	}
 	.javascript_content {
-		border:solid rgb(204, 255, 136);
+		background: transparent;
+		padding: 10%;
+		text-align: start;
 	}
 	.jquery {
 		flex-basis: 40%;
 		position: relative;
+		display: block;
+    padding: 0.5em 1em;
+    text-decoration: none;
+    background: #f7f7f7;
+    border-top: solid 6px #ff7c5c;/*左線*/
+    color: #ff7c5c;/*文字色*/
+    font-weight: bold;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
 		margin-top: 5%;
 	}
 	.jquery_top {
-		position: absolute;
-		top: -4%;
-		width: 40%;
-		left: 30%;
+		background: transparent;
 	}
 	.jquery_content {
-		border:solid rgb(255, 230, 0);
+		background: transparent;
+		padding: 10%;
+		text-align: start;
 	}
 	.vue {
 		flex-basis: 40%;
 		position: relative;
+		display: block;
+    padding: 0.5em 1em;
+    text-decoration: none;
+    background: #f7f7f7;
+    border-top: solid 6px #ff7c5c;/*左線*/
+    color: #ff7c5c;/*文字色*/
+    font-weight: bold;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
 		margin-top: 5%;
 	}
 	.vue_top {
-		position: absolute;
-		top: -4%;
-		width: 30%;
-		left: 35%;
+		background: transparent;
 	}
 	.vue_content {
-		border:solid rgb(7, 2, 75);
+		background: transparent;
+		padding: 10%;
+		text-align: start;
 	}
-
+	h2 {
+		text-align: center;
+    width: 60%;
+		position: relative;
+    display: inline-block;
+    padding: 0 5px;
+		margin-bottom: 5%;
+		color: #ff4618;
+	}
+	h2::before {
+		position: absolute;
+    content: '';
+    top: 0;
+    right: 5px;
+    width: 5px;
+    height: 100%;
+    border: 5px solid #ff7c5c;
+    border-left: none;
+	}
+ h2::after {
+		position: absolute;
+    content: '';
+    top: 0;
+    left: 5px;
+    width: 5px;
+    height: 100%;
+    border: 5px solid #ff7c5c;
+    border-right: none;
+		margin-bottom: 5%;
+	}
+	h2 {
+		font-size: 2rem;
+		margin-top: 5%;
+	}
 	.server {
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: space-between;
 	}
+	
 	.ruby {
 		flex-basis: 40%;
 		position: relative;
+		display: block;
+    padding: 0.5em 1em;
+    text-decoration: none;
+    background: #f7f7f7;
+    border-top: solid 6px #ff7c5c;/*左線*/
+    color: #ff7c5c;/*文字色*/
+    font-weight: bold;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
 		margin-top: 5%;
 	}
 	.ruby_top {
-		position: absolute;
-		top: -4%;
-		width: 30%;
-		left: 35%;
+		background: transparent;
 	}
 	.ruby_content {
-		border:solid rgb(7, 2, 75);
+		background: transparent;
+		padding: 10%;
+		text-align: start;
 	}
 	.mysql {
 		flex-basis: 40%;
 		position: relative;
+		display: block;
+    padding: 0.5em 1em;
+    text-decoration: none;
+    background: #f7f7f7;
+    border-top: solid 6px #ff7c5c;/*左線*/
+    color: #ff7c5c;/*文字色*/
+    font-weight: bold;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
 		margin-top: 5%;
 	}
 	.mysql_top {
-		position: absolute;
-		top: -4%;
-		width: 30%;
-		left: 35%;
+		background: transparent;
 	}
 	.mysql_content {
-		border:solid rgb(7, 2, 75);
+		background: transparent;
+		padding: 10%;
+		text-align: start;
 	}
 </style>
